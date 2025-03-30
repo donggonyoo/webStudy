d390d7872319ffa0bbeccd8974d1cf563d687477
donggonyoo <ddkk8525@gmail.com>
2025-03-30 17:32:44 +0900

    ㅎㅎㅎ

--- javascript/20250326test/test4.js
+++ javascript/20250326test/test4.js
@@ -1,4 +1,14 @@
-function test1(){
+// function test1(){
+//     let prom = prompt("메세지를 입력하세요");
+//     if(prom.length ==0 ){
+//         alert("메시지가 입력되지않았어요")
+//     }
+//     else{
+//         document.querySelector("#test1").innerHTML = prom;
+//     }
+// }
+let but = document.getElementsByTagName("button")[0];
+but.onclick = function(){
     let prom = prompt("메세지를 입력하세요");
     if(prom.length ==0 ){
         alert("메시지가 입력되지않았어요")
@@ -7,18 +17,20 @@ function test1(){
         document.querySelector("#test1").innerHTML = prom;
     }
 }
+//-----------------------------------------------------------------------
 
 function test2(){
     let con = confirm("개인정보 동의?");
     if(con){ //con은 확인 :true 반환 // 취소 : false 반환
-        document.querySelector("#test2").innerHTML = "개인정보가유출";
+        $("#test2").html("개인정보가유출"); //jQuery방식
+// document.querySelector("#test2").innerHTML = "개인정보유출"
     }
     else{
-        document.querySelector("#test2").innerHTML = "당신의 개인정보 유출을 막았습니다";
+        $("#test2").html("당신의 개인정보 유출을 막았습니다"); 
 
     }
 }
-
+//-----------------------------------------------------------------------
 function test3(){
     let arr = [];
     while(arr.length <= 10){
@@ -31,20 +43,26 @@ function test3(){
             arr.push(ran);
         } 
     }
-   arr.sort((left,right)=>{return -1*(left - right)});
+   arr.sort((a,b)=>{return -1*(a - b)});
    //배열은 숫자로 정렬이 제대로되지않음(아스키코드)
    let u = document.querySelector("#test3>ul");
+   document.ul.appendChild()
    for(let i=0 ; i<arr.length ; i++){
     u.innerHTML += `<li>${arr[i]}</li>`
    }
 }
-
+//-----------------------------------------------------------------------
 function test4(){
     let imText = document.querySelector("input[name='img']").value;
     document.querySelector(".img-container").innerHTML+=
     (`<img src='${imText}' witdth="200" height="200">`);
-}
 
+    //jQuery방식
+    // let imText = $("input[name='img']").val();
+    // $(".img-container")
+    // .html(`<img src='${imText}' witdth="200" height="200">`);
+}
+//-----------------------------------------------------------------------
 function test5(){
     let data = document.querySelectorAll(".data-container>*");
     let result = document.querySelector("#checkStr");
@@ -58,11 +76,8 @@ function test5(){
      //복제할요소객체.cloneNode(논리값) : 복제된요소객체
      let copy = data[i].cloneNode(true); 
      // true : textNode까지 모두복제 / false : elementNode만복제
-     result.appendChild(copy);
+     result.appendChild(copy);//#checkStr의 하위에 집어넣기
     }
-   }
-
-
-   
+   } 
 }
 
---
+++ javascript/20250327test/test8.js
@@ -0,0 +1,48 @@
+let text = document.querySelector("input[type='text']");
+let but = document.querySelectorAll("input[type='button']");
+
+for(i=0;i<but.length;i++){ //모든루프를 돌린다
+    but[i].onclick=function(){
+        //모든(button)배열에 click이벤트발생시 함수를 지정
+        let value = this.value;//자신의 value를 담는 변수
+            if(value=='AC'){ //value가 AC면 0으로초기화
+                text.value=0;
+            }
+            else if(value=='='){
+                text.value = eval(text.value);
+            }
+            else{
+                if(text.value==0){ 
+                    //처음text의 value==0 일시
+                    //내가 클릭한value로 변경
+                    text.value = value;
+                }
+                else{
+                    text.value += value;
+                }
+                
+            }
+    };
+}
+
+// but.forEach(function(button){ //모든버튼가져오기
+//     button.addEventListener("click", function(){ //모든버튼에 click이벤트추가
+//         let value = this.value;
+//         if(value=='AC'){
+//             text.value=0;
+//         }
+//         else if(value=='='){
+//             text.value = eval(text.value);
+//         }
+//         else{
+//             if(text.value==0){
+//                 text.value = value;
+//             }
+//             else{
+//                 text.value += value;
+//             }
+            
+//         }
+//     })
+// })
+
